2f9abdc92a8147473ee77978cdc9240dceae296a
nl1cs <ntna102@gmail.com>
2022-08-12 18:36:56 +0700

    scrollin

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,35 @@
 const dropdownBtn = document.querySelector('.dropdown');
+const dropdown = document.querySelector('.dropdown-content');
 dropdownBtn.onclick = function(){
-  $('.dropdown-content').toggleClass('working')
+  dropdown.classList.toggle("working");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('working')) {
+        openDropdown.classList.remove('working');
+      }
+    }
+  }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
